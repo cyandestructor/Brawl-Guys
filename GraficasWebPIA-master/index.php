4f4b1955491368bf3d2ecd0927c9d9a71e2b6d1e
AlbertoGzzz <84883567+AlbertoGzzz@users.noreply.github.com>
2021-10-17 13:13:58 -0500

    Add files via upload
    Menu inicial ya con botones separados, y si le picas play te lleva directo a la pantalla de juego.
    Hoy en la noche termino de acomodar las ventanas de settings y las que faltan

---
+++ GraficasWebPIA-master/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		$link = "http://localhost/GraficasWebPIA/GraficasWebPIA-master/";
+	?>
+	<link rel="shortcut icon" type="image" href="<?php echo $link; ?>media/images/ICON-31.png"/>
+
+	 <link rel="stylesheet" href="css/design.css"/>
+	<title>
+		Brawl Guys
+	</title>
+</head>
+<!--<body>
+<img class="bg" src="bg.gif" alt="Italian Trulli">
+</body>-->
+
+
+	<body>
+
+
+			<audio autoplay>
+				<source src="<?php echo $link; ?>media/sounds/menum.mp3" type="audio/mp3">
+			</audio>
+
+			<div class="container">
+
+				<img src="<?php echo $link; ?>media/images/GAME UI-34-01.png" alt="Italian Trulli">
+
+				<a href="<?php echo $link; ?>pages/game.php"><img src="<?php  echo $link; ?>media/images/GAME UI-33.png" alt="Italian Trulli" id="btn"></a>
+
+
+				<!--<a href="scoreboards.html"><button id="btn2">Button2</button></a>-->
+
+				<a href="<?php echo $link; ?>pages/scoreboards.php"><img src="<?php  echo $link; ?>media/images/GAME UI-34.png" alt="Italian Trulli" id="btn2"></a>
+
+				<a href="<?php echo $link; ?>pages/scoreboards.php"><img src="<?php  echo $link; ?>media/images/GAME UI-35.png" alt="Italian Trulli" id="btn3"></a>
+
+
+				<a href="<?php echo $link; ?>pages/scoreboards.php"><img src="<?php  echo $link; ?>media/images/GAME UI-36.png" alt="Italian Trulli" id="btn4"></a>
+
+				<a href="<?php echo $link; ?>pages/scoreboards.php"><img src="<?php  echo $link; ?>media/images/GAME UI-37.png" alt="Italian Trulli" id="btn5"></a>
+
+				
+
+			</div>
+
+		<a href="<?php echo $link; ?>pages/settings.php"><img class="ui" src="<?php echo $link; ?>media/images/GAME UI-09.png" alt="Italian Trulli"></a>
+
+		<img class="ui" src="<?php echo $link; ?>media/images/GAME UI-16.png" alt="Italian Trulli">
+
+
+	</body>
+
+</html>
---
+++ GraficasWebPIA-master/pages/play.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		$link = "http://localhost/GraficasWebPIA/GraficasWebPIA-master/";
+	?>
+
+	 <link rel="shortcut icon" type="image" href="<?php echo $link; ?>media/images/ICON-31.png"/>
+
+	 <link rel="stylesheet" href="css/design.css"/>
+
+	<title>Select map</title>
+</head>
+<body>
+
+	
+
+	<img class="menu" src="<?php echo $link; ?>media/images/GAME UI-05.png" alt="Italian Trulli">
+
+	<a href="<?php echo $link; ?>pages/settings.php"><img class="ui" src="<?php echo $link; ?>media/images/GAME UI-09.png" alt="Italian Trulli"></a>
+	
+	<img class="ui" src="<?php echo $link; ?>media/images/GAME UI-16.png" alt="Italian Trulli">
+
+</body>
+</html>
