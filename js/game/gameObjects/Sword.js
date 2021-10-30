8d7b6c629411acc8c0cf074dc7fcd2a12e1fd45e
Bryan Duarte <stfrisephone@gmail.com>
2021-10-29 20:15:27 -0500

    Added behaviour to the Sword

--- js/game/gameObjects/Sword.js
+++ js/game/gameObjects/Sword.js
@@ -5,8 +5,16 @@ import PickableItem from "./PickableItem.js";
 
 export default class Sword extends PickableItem {
 
+    holder = null;
+    hitbox = {};
+
+    attackBonus;
+    attackCooldown = 0;
+
     constructor(scene, props = {}) {
         super(scene, props.pickedUp ?? false);
+
+        this.attackBonus = props.attackBonus ?? 2;
 
         this.initModel(props);
     }
@@ -24,7 +32,32 @@ export default class Sword extends PickableItem {
             pivot.position.copy(props.position);
         }
 
+        this.attachHitbox(object);
         this.handler = pivot;
+    }
+
+    attachHitbox(object) {
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00,
+            transparent: true,
+            opacity: 0.5
+        });
+        const collisionBoxHelper = new THREE.Mesh( geometry, material );
+        collisionBoxHelper.geometry.computeBoundingBox();
+        collisionBoxHelper.visible = false;
+
+        const hitboxMesh = collisionBoxHelper.clone();
+        hitboxMesh.scale.set(15, 135, 20);
+        hitboxMesh.position.y = 130;
+
+        this.hitbox = {
+            mesh: hitboxMesh,
+            box: new THREE.OBB(),
+            active: false
+        };
+
+        object.add(hitboxMesh);
     }
     
     getActionCharacterState() {
@@ -42,6 +75,14 @@ export default class Sword extends PickableItem {
                 this.checkForInteraction();
             }
         }
+        else {
+            this.updateBoundingBox();
+            this.updateCollisions(dt);
+        }
+
+        this.attackCooldown -= dt;
+        this.attackCooldown = Math.max(this.attackCooldown, 0);
+        
         super.onUpdate(dt);
     }
 
@@ -50,7 +91,13 @@ export default class Sword extends PickableItem {
         this.handler.children[0].position.set(0, 0, 0);
         this.handler.rotation.y = THREE.Math.degToRad(90);
         triggerObject.setCurrentItem(this);
+        this.holder = triggerObject;
         super.onInteraction(triggerObject);
+    }
+
+    onUse(triggerObject, beginUse = true) {
+        this.hitbox.active = beginUse;
+        return this.getActionCharacterState();
     }
 
     onDrop(position) {
@@ -58,6 +105,28 @@ export default class Sword extends PickableItem {
         this.handler.position.copy(position);
         this.handler.children[0].position.y = 35;
         this.handler.rotation.y = 0;
+        this.holder = null;
         super.onDrop(position);
+    }
+
+    updateBoundingBox() {
+        this.hitbox.box.fromBox3(this.hitbox.mesh.geometry.boundingBox);
+        this.hitbox.box.applyMatrix4(this.hitbox.mesh.matrixWorld);
+    }
+
+    updateCollisions(dt) {
+        // Update collisions with players
+        for (const player of Character.totalPlayers) {
+            if (player.playerIndex != this.holder.playerIndex && !player.isDeath) {
+                if (this.hitbox.active
+                        && this.attackCooldown <= 0
+                        && this.hitbox.box.intersectsOBB(player.hurtBox.box)
+                    ) {
+                    this.attackCooldown = 0.5;
+                    player.onDamage(dt, this.holder.direction, this.holder.attackPower * this.attackBonus);
+                    // break; // Maybe needed
+                }
+            }
+        }
     }
 }
